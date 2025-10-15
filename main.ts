4f119b1ff2f10a8bf0a8d45b6f3579564d0aaa64
jack-mcnulty <jack.mcnulty@stu.ocsb.ca>
2025-10-15 12:58:21 -0400

    added code

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack
+ * Created on: Oct 2025
+ * This program gets the distance from a sensor
 */
 
-basic.showString('Hello, World!')
+//Variables
+let distanceToObject: number = 0
+
+//Setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//Getting distance
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    distanceToObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+)
+})
